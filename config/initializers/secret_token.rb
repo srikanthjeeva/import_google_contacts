b0b9145dbc18f4551e5503f0e89158974c3a8417
Srikanth <sri.jjhero@gmail.com>
2012-10-15 22:23:30 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleContacts::Application.config.secret_token = '1da412f176fcd48b46c6e8b7c657d5abc9ac21b2d5d147fa894b4603a811fae08ed9fb1b53571d28bc64d1608428ef266e7cbf4749e79ae693b37607748bcc82'
